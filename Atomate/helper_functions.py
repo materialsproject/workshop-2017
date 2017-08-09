fd2facc9e05470df61d08fe332efb1e00c265a4a
Kiran Mathew <kmathew@lbl.gov>
2017-08-09 16:34:05 -0700

    cleanup

--- Atomate/helper_functions.py
+++ Atomate/helper_functions.py
@@ -2,32 +2,36 @@ import os
 from atomate.vasp.powerups import use_fake_vasp
 
 
-def electronic_structure_simulator(wf, ref_dir="/wkshp_shared/workshop_ref_files/electronic_structure"):
-    reference_dir = os.path.expanduser(ref_dir)
-    ref_dirs_si = {
-        "structure optimization": os.path.join(reference_dir, "Si_structure_optimization"),
-        "static": os.path.join(reference_dir, "Si_static"),
-        "nscf uniform": os.path.join(reference_dir, "Si_nscf_uniform"),
-        "nscf line": os.path.join(reference_dir, "Si_nscf_line")
-    }
+def vasp_emulator(wf, ref_dir="/wkshp_shared/workshop_ref_files", calc="electronic_structure"):
+    reference_dir = os.path.join(os.path.expanduser(ref_dir), calc)
+    params_to_check = None
     
-    return use_fake_vasp(wf, ref_dirs_si)
+    if calc == "electronic_structure":
 
+        ref_dirs = {
+            "structure optimization": os.path.join(reference_dir, "Si_structure_optimization"),
+            "static": os.path.join(reference_dir, "Si_static"),
+            "nscf uniform": os.path.join(reference_dir, "Si_nscf_uniform"),
+            "nscf line": os.path.join(reference_dir, "Si_nscf_line")
+        }
+        
+    elif calc == "elastic_tensor":
 
-def elastic_simulator(wf, ref_dir="/wkshp_shared/workshop_ref_files/elastic_tensor"):
-    reference_dir = os.path.expanduser(ref_dir)
+        ref_dirs = {
+            "structure optimization": os.path.join(reference_dir, "1"),
+            "elastic deformation 0": os.path.join(reference_dir, "7"),
+            "elastic deformation 1": os.path.join(reference_dir, "6"),
+            "elastic deformation 2": os.path.join(reference_dir, "5"),
+            "elastic deformation 3": os.path.join(reference_dir, "4"),
+            "elastic deformation 4": os.path.join(reference_dir, "3"),
+            "elastic deformation 5": os.path.join(reference_dir, "2")
+        }
+        params_to_check=["ENCUT"]
 
-    si_ref_dirs = {
-        "structure optimization": os.path.join(reference_dir, "1"),
-        "elastic deformation 0": os.path.join(reference_dir, "7"),
-        "elastic deformation 1": os.path.join(reference_dir, "6"),
-        "elastic deformation 2": os.path.join(reference_dir, "5"),
-        "elastic deformation 3": os.path.join(reference_dir, "4"),
-        "elastic deformation 4": os.path.join(reference_dir, "3"),
-        "elastic deformation 5": os.path.join(reference_dir, "2")
-    }
-
-    return use_fake_vasp(wf, si_ref_dirs, params_to_check=["ENCUT"])
+    else:
+        raise ValueError("wrong 'calc' option")
+        
+    return use_fake_vasp(wf, ref_dirs, params_to_check=params_to_check)
 
 
 def get_task_id(lp, fw_name):
